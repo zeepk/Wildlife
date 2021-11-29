449c69214362dcea5e5cd9c96572d97d15741f5d
Matt Hughes <zeepkdb@gmail.com>
2021-11-28 23:09:58 -0800

    updated redirect

--- src/index.tsx
+++ src/index.tsx
@@ -11,7 +11,7 @@ ReactDOM.render(
 	<Auth0Provider
 		domain={process.env.REACT_APP_AUTH0_DOMAIN || ''}
 		clientId={process.env.REACT_APP_AUTH0_CLIENT_ID || ''}
-		redirectUri="http://localhost:3000"
+		redirectUri={window.location.href}
 	>
 		<React.StrictMode>
 			<Provider store={store}>
