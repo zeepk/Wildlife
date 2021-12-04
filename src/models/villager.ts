a41d2d61f8a447d1edfa6dcca462c1f747c1f88b
Matthew Hughes <42755431+zeepk@users.noreply.github.com>
2021-12-03 22:14:17 -0800

    added euid to villager model

--- src/models/villager.ts
+++ src/models/villager.ts
@@ -8,6 +8,7 @@ export interface IVillager extends Document {
 	gender: string;
 	personality: string;
 	birthday: string;
+	euid: string;
 }
 
 const VillagerSchema = new Schema<IVillager>(
@@ -20,6 +21,7 @@ const VillagerSchema = new Schema<IVillager>(
 		gender: String,
 		personality: String,
 		birthday: String,
+		euid: String,
 	},
 	{
 		timestamps: {
