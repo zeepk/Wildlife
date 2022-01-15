deffafaa2995a42b8fb8b2900e14cee5a8e390b4
Matthew Hughes <42755431+zeepk@users.noreply.github.com>
2022-01-15 01:35:54 -0800

    token clg

--- web/src/features/Common/Auth/Login.tsx
+++ web/src/features/Common/Auth/Login.tsx
@@ -8,6 +8,8 @@ export function Login() {
 	const { id }: any = useParams();
 
 	setCookie('login_jwt', id.toString(), 9);
+	console.log('token:');
+	console.log(id.toString());
 	return (
 		<div>
 			<h1>woah</h1>
